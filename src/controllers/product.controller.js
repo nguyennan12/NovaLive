f1df74cd23fbc8f284080bc8653c56a9de76832c
nguyenan12 <nguyenann1204@gmail.com>
2026-03-28 20:43:08 +0700

    published, get(publish, draft, all, detail), search product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -22,7 +22,70 @@ const updateProduct = async (req, res, next) => {
   }).send(res)
 }
 
+const publishProduct = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Published product successfully!',
+    metadata: await ProductFactory.publishProduct({ productId: req.params.productId, shopId: req.user.userId })
+  }).send(res)
+}
+
+const unPublishProduct = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Unpublished product successfully!',
+    metadata: await ProductFactory.unPublishProduct({ productId: req.params.productId, shopId: req.user.userId })
+  }).send(res)
+}
+
+const getPublishedProduct = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Get list published product successfully!',
+    metadata: await ProductFactory.getPublishedProduct({ shopId: req.user.userId })
+  }).send(res)
+}
+
+const getDraftProduct = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Get list draft product successfully!',
+    metadata: await ProductFactory.getDraftProduct({ shopId: req.user.userId })
+  }).send(res)
+}
+
+const getAllProducts = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Get list product successfully!',
+    metadata: await ProductFactory.getAllProducts(req.query)
+  }).send(res)
+}
+
+const getProductDetail = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Get product successfully!',
+    metadata: await ProductFactory.getProductDetail({ productId: req.params.productId })
+  }).send(res)
+}
+
+const searchProduct = async (req, res, next) => {
+  new ApiSuccess({
+    statusCode: StatusCodes.OK,
+    message: 'Search product successfully!',
+    metadata: await ProductFactory.searchProduct(req.query)
+  }).send(res)
+}
+
 export default {
   createProduct,
-  updateProduct
+  updateProduct,
+  publishProduct,
+  unPublishProduct,
+  getPublishedProduct,
+  getDraftProduct,
+  getAllProducts,
+  getProductDetail,
+  searchProduct
 }
