c236db37ec751f591b0f5be44fe0e5df302a1df8
nguyenan12 <nguyenann1204@gmail.com>
2026-04-01 21:43:11 +0700

    config docker for project

--- src/routes/v1/product.router.js
+++ src/routes/v1/product.router.js
@@ -9,14 +9,14 @@ const Router = express.Router()
 // === User ===
 Router.get('/search/', asyncHandler(productController.searchProduct))
 Router.get('', asyncHandler(productController.getAllProducts))
-Router.get('/:productId', asyncHandler(productController.getProductDetail))
+Router.get('/detail/:productId', asyncHandler(productController.getProductDetail))
 //  === Authentication ===
 Router.use(authentication)
 // === Shop ===
 Router.post('', validate(productValidation.create), asyncHandler(productController.createProduct))
-Router.patch('/:productId', validate(productValidation.paramsProductId), asyncHandler(productController.updateProduct))
-Router.patch('/:productId/publish', validate(productValidation.paramsProductId), asyncHandler(productController.publishProduct))
-Router.patch('/:productId/unpublish', validate(productValidation.paramsProductId), asyncHandler(productController.unPublishProduct))
+Router.patch('/:productId', asyncHandler(productController.updateProduct))
+Router.patch('/:productId/publish', asyncHandler(productController.publishProduct))
+Router.patch('/:productId/unpublish', asyncHandler(productController.unPublishProduct))
 Router.get('/public', asyncHandler(productController.getPublishedProduct))
 Router.get('/draft', asyncHandler(productController.getDraftProduct))
 
