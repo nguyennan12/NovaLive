f46ec9ed6164e305fa9aa66134560aff1147acc3
nguyenan12 <nguyenann1204@gmail.com>
2026-04-09 13:51:55 +0700

    some comment at inventory

--- src/services/inventory.service.js
+++ src/services/inventory.service.js
@@ -9,11 +9,14 @@ import { PREFIX } from '#utils/constant.js'
 import fileHelper from '#helpers/file.helper.js'
 import { RabbitMQClient } from '#database/init.rabbitMQ.js'
 
+//lua script
 const reserveStockScript = fileHelper.loadLuaScript('lua/reserveStock.lua')
 
 const addStockToInventory = async ({ shopId, reqBody }) => {
   const { productId, skuId, stock } = reqBody
   if (!stock || stock < 0) throw new ApiError(StatusCodes.BAD_REQUEST, 'Stock invalid')
+
+  //tìm product và sku tương ứng
   const [foundSku, foundProduct] = await Promise.all([
     skuModel.findOne({ sku_spuId: productId, sku_id: skuId }).lean(),
     spuRepo.findProductDetail(productId)
@@ -21,6 +24,8 @@ const addStockToInventory = async ({ shopId, reqBody }) => {
   if (!foundSku || !foundProduct) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Product does not exists')
   }
+
+  //thêm stock vào inventory
   const newInven = await inventoryModel.findOneAndUpdate(
     {
       inven_shopId: converter.toObjectId(shopId),
@@ -35,6 +40,7 @@ const addStockToInventory = async ({ shopId, reqBody }) => {
   const prefix = `${PREFIX.INVENTORY_SKU}:${skuId}:stock`
   const ivenExistsCache = await redisClient.exists(prefix)
 
+  //kiểm tra xem product hiện tại có hot không, nếu có thì lưu cache
   const isHot = foundProduct.live?.is_live || newInven.inven_stock < 10
   if (isHot || ivenExistsCache) {
     const ttl = 60 * 60
@@ -43,6 +49,7 @@ const addStockToInventory = async ({ shopId, reqBody }) => {
   return newInven
 }
 
+//kiểm tra stock của 1 product có hợp lệ để lấy ra bán k
 const checkAvailableStock = async ({ skuId, quantity }) => {
   const prefix = `${PREFIX.INVENTORY_SKU}:${skuId}:stock`
   const stockCache = await redisClient.get(prefix)
@@ -50,8 +57,10 @@ const checkAvailableStock = async ({ skuId, quantity }) => {
 
   const foundStock = await inventoryModel.findOne({ inven_skuId: skuId })
   if (!foundStock) return false
+  //stock hợp lệ bằng stock trong kho có trừ đi stock đang đc tạm giữ (có thể đang order)
   const availableStock = foundStock.inven_stock - foundStock.inven_reserved
 
+  //hợp lệ thì lưu vào cache
   if (availableStock) {
     const ttl = 60 * 60
     await redisClient.set(prefix, availableStock, 'EX', ttl)
@@ -68,14 +77,18 @@ const reserveStock = async ({ userId, orderId, items }) => {
     args.push(items.quantity.toString())
   })
 
+  //tạp 1 transactions để đảm bảo tính trọn vẹn của stock khi đang trong quá trình order
+  //và trừ đi số lượng trong kho nếu stock hợp lệ
   const result = await redisClient.eval(reserveStockScript, keys.length, ...keys, ...args)
   if (result === 0) throw new ApiError(StatusCodes.BAD_REQUEST, 'Have a product is sold out! please check again')
 
+  //gửi message vào hàng đợi message_queue để nó xử lý order
   const payload = { action: 'RESERVE_DB', orderId, userId, items }
   await RabbitMQClient.publishMessage('inventory_queue', payload)
   return { status: 'SUCCESS', message: 'Order Successfully' }
 }
 
+//nhã kho hàng
 const releaseStock = async (orderId, items) => {
   if (!items || items.length === 0) throw new ApiError(StatusCodes.BAD_REQUEST, 'Cart is blank!')
   const bulkOperations = items.map(item => {
@@ -93,7 +106,7 @@ const releaseStock = async (orderId, items) => {
     }
   })
   const result = await inventoryModel.bulkWrite(bulkOperations)
-
+  //hhi nhã xong cập nhật là redis  stock dc nhã ra cho product
   await Promise.all(items.map(async (item) => {
     const prefix = `${PREFIX.INVENTORY_SKU}:${skuId}:stock`
     const exists = await redisClient.exists(prefix)
@@ -106,6 +119,7 @@ const releaseStock = async (orderId, items) => {
   }
 }
 
+//khi thanh toán xong thì trừ đi luôn stock
 const confirmDeductStock = async (orderId, items) => {
   const bulkOperations = items.map(item => {
     return {
